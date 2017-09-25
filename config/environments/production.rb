35d22196840ff46741fde47f8aab99a8bc5e26e0
Erin McLaughlin <emclaughlin@bostoninteractive.com>
2017-09-25 14:43:33 -0400

    Fixing a type that was crashing the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "https://twoisforjoy.com" }
 
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   #TODO: Add real prod settings
   config.action_mailer.smtp_settings =
