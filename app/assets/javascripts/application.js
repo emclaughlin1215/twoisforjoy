8a707f4806c834baa4a5ebdedbb3885225c8d161
Erin McLaughlin <emclaughlin@bostoninteractive.com>
2017-09-26 08:32:12 -0400

    Will changing the order of being added help?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery_ujs
 //= require rails-ujs
 //= require foundation
 //= require turbolinks
-//= require jquery3
-//= require jquery_ujs
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
