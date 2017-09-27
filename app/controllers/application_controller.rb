094f2922f3b3ea766d367422843be53bf857aab0
Erin McLaughlin <emclaughlin@bostoninteractive.com>
2017-09-27 16:42:05 -0400

    Playing around with the hero.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  attr_accessor :hero_path, :title
+  attr_accessor :hero_path, :title, :hero_heading, :hero_text, :hero_alignment
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
-  	@hero_path = 'snow-1120800.jpg'
+  	@hero_path = '/heros/snow-1120800.jpg'
+  	@hero_heading = "Erin McLaughlin"
+  	@hero_text = "Certified Drupal and Rails Web Developer"
+  	@hero_alignment = 'left'
     @posts = Post.all.limit(3).order("created_at desc")
     @projects = Project.all.limit(3).order("created_at desc")
   end
