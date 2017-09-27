184424043325c087da31de89f31cd89341fe0d69
Erin McLaughlin <emclaughlin@bostoninteractive.com>
2017-09-27 15:08:07 -0400

    Fixing my local so the DB works and adding a hero image.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+  	@hero_path = 'snow-1120800.jpg'
     @posts = Post.all.limit(3).order("created_at desc")
     @projects = Project.all.limit(3).order("created_at desc")
   end
