17e5733f8a474125a7e3881a450c93c3ebe353ed
Erin McLaughlin <emclaughlin1215@gmail.com>
2018-04-17 16:49:16 -0400

    A bit more, for editing.

--- twoisforjoy/app/controllers/users_controller.rb
+++ twoisforjoy/app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
