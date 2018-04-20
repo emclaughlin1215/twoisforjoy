06a7df940d78ae1896973ac149b5c1d31534d5bc
Erin McLaughlin <emclaughlin1215@gmail.com>
2018-04-20 10:30:27 -0400

    One rspec test.

---
+++ twoisforjoy/spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :user do
+  before(:each) do
+    @user = described_class.new(name: "Erin McLaughlin", 
+                                email: "test@example.com", 
+                                password: "testtest", 
+                                password_confirmation: "testtest")
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user).to be_valid
+  end
+
+  it "is not valid with blank name" do
+    @user.name = ""
+    expect(@user).to_not be_valid
+  end
+
+  it "is not valid with mismatched passwords" do
+    @user.password = "testest"
+    @user.password_confirmation = "testtest"
+    expect(@user).to_not be_valid
+  end
+
+  it "is not valid with blank email" do
+    @user.email = ""
+    expect(@user).to_not be_valid
+  end
+end
